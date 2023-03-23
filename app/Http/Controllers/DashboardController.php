63f6975e1c623b84fb9899030a6faa2bd0bb29ee
elfeny7 <ikmalfaris50@gmail.com>
2023-03-24 01:42:15 +0700

    change route service provider and add auth dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
-        return view('dashboard');
+        $user = Auth::user();
+        return view('dashboard', ['user'=>$user]);
     }
     public function redirect() {
         return redirect()->route('login');
